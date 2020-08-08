55de374ee192ea6dfbd969df01ce97744020bb06
Conrad Buys <conradbuys11@gmail.com>
2020-08-08 18:40:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -68,6 +68,10 @@ def adding_matz
 
 	programmer_hash = 
  		{
+ 		  :yukihiro_matsumoto => {
+ 		    :known_for => "Ruby"
+ 		    :languages => ["LISP", "C"]
+ 		  }
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -81,6 +85,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
+  
 
 end
 
